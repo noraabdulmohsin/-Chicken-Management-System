a8c8867426a17517fb803f223d1721328f50c498
noraabdulmohsin <noraabdulmohsin@gmail.com>
2026-03-27 06:34:09 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,15 @@
+
+package chicken;
+
+public class Chicken {
+
+    public static void main(String[] args) {
+      theChicken C1=new theChicken(1,2.5) ;
+        C1.ChikenDisplay();
+      theChicken.setDailyPrice(10);
+       C1.ChikenDisplay();
+       theChicken.setNecessaryWeight(3);
+       C1.ChikenDisplay();
+    }
+    
+}
